64a3c488690f811b4d36b6f24953d8c9c815d69b
Rj-Aquino <richardjasonmaquino@iskolarngbayan.pup.edu.ph>
2025-04-05 01:32:54 +0800

    7

--- ADET/asgi.py
+++ ADET/asgi.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+import resource
+resource.setrlimit(resource.RLIMIT_AS, (450 * 1024 * 1024, 450 * 1024 * 1024))  # Hard limit
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ADET.settings')
 
