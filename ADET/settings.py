3d2d7d5a7e9553cde2b815f6a85b412e79a1767a
Rj-Aquino <richardjasonmaquino@iskolarngbayan.pup.edu.ph>
2025-04-05 01:06:37 +0800

    5

--- ADET/settings.py
+++ ADET/settings.py
@@ -25,14 +25,15 @@ SECRET_KEY = 'django-insecure-_9*0@zv#ia)d#d!=&s*%hl_&=$nzj3l_tjzmp)o$186x8-6z_5
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = False  # Disable debug mode in production!
-ALLOWED_HOSTS = ["your-app.up.railway.app", "localhost", 
+ALLOWED_HOSTS = ["your-app.up.railway.app", "localhost",
+                "https://adet-project-production.up.railway.app", 
                  "127.0.0.1"]  # Add Railway's URL
 CSRF_TRUSTED_ORIGINS = ["https://your-app.up.railway.app"]  # For HTTPS security
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')  # Force HTTPS
 
 STATIC_URL = '/static/'
 
-CSRF_TRUSTED_ORIGINS= [""]
+CSRF_TRUSTED_ORIGINS= ["https://adet-project-production.up.railway.app"]
 
 # Application definition
 
